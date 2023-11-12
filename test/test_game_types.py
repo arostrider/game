bc7e6c47a4a9381b8e78f0dae86f5fadec23058c
Marko <mlaketic@outlook.com>
2023-11-12 01:18:00 +0100

    test sprite

--- test/test_game_types.py
+++ test/test_game_types.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.game_types import Drawable
+from src.game_types import Drawable, Sprite
 import pygame
 
 
@@ -22,7 +22,8 @@ class DrawableChild(Drawable):
         raise NotImplemented
 
 
-COORDINATES = [(i, j) for i in range(-128, 128, 32) for j in range(-128, 128, 32)]
+COORDINATES = [(i, j) for i in range(-64, 64, 32) for j in range(-64, 64, 32)]
+COORDINATES.extend([(i, j) for i in range(-4, 4) for j in range(-4, 4)])
 
 
 @pytest.mark.parametrize(("width", "height"), COORDINATES)
@@ -30,4 +31,31 @@ COORDINATES = [(i, j) for i in range(-128, 128, 32) for j in range(-128, 128, 32
 def test_drawable(width, height, x, y):
     drawable = DrawableChild(image="placeholder", width=width, height=height)
 
+    assert drawable.image == "placeholder"
     assert drawable.desired_on_screen_position(x, y) == pygame.Rect(x, y, drawable.width, drawable.height)
+
+
+class MockImage:
+    def __init__(self, width: int, height: int):
+        super().__init__()
+        self._width = width
+        self._height = height
+
+    def get_width(self) -> float:
+        return self._width
+
+    def get_height(self) -> float:
+        return self._height
+
+
+@pytest.mark.parametrize(("width", "height"), COORDINATES)
+@pytest.mark.parametrize(("x", "y"), COORDINATES)
+def test_sprite(width, height, x, y):
+    image = MockImage(width=width, height=height)
+    sprite = Sprite(image=image)
+
+    assert sprite.width == width
+    assert sprite.height == height
+
+    assert sprite.draw_kwargs(x, y) == {"source": image,
+                                        "dest": sprite.desired_on_screen_position(x, y)}
