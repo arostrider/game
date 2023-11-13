0e37751489b8e1c670a7f78f8f62035bab43e2e7
Marko <mlaketic@outlook.com>
2023-11-13 23:11:15 +0100

    test_sprite_sheet

--- test/test_game_types.py
+++ test/test_game_types.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.game_types import Drawable, Sprite
+from src.game_types import Drawable, Sprite, SpriteSheet
 import pygame
 
 
@@ -23,7 +23,7 @@ class DrawableChild(Drawable):
 
 
 COORDINATES = [(i, j) for i in range(-64, 64, 32) for j in range(-64, 64, 32)]
-COORDINATES.extend([(i, j) for i in range(-4, 4) for j in range(-4, 4)])
+COORDINATES.extend([(i, j) for i in range(-1, 1) for j in range(-1, 1)])
 
 
 @pytest.mark.parametrize(("width", "height"), COORDINATES)
@@ -61,5 +61,23 @@ def test_sprite(width, height, x, y):
                                         "dest": sprite.desired_on_screen_position(x, y)}
 
 
-def test_sprite_sheet():
-    ...
+@pytest.mark.parametrize(("width", "height"), [(128, 192)])
+@pytest.mark.parametrize(("columns", "rows"), [(4, 4)])
+@pytest.mark.parametrize(("x", "y"), COORDINATES)
+@pytest.mark.parametrize(("subsprite_column", "subsprite_row"), COORDINATES)
+def test_sprite_sheet(width, height, columns, rows, x, y, subsprite_column, subsprite_row):
+    image = MockImage(width=width, height=height)
+    sprite_sheet = SpriteSheet(image=image, columns=4, rows=4)
+
+    assert sprite_sheet.width == width / columns
+    assert sprite_sheet.height == height / rows
+
+    assert sprite_sheet.curr_subsprite() == pygame.Rect(sprite_sheet.curr_column * sprite_sheet.width,
+                                                        sprite_sheet.curr_row * sprite_sheet.height,
+                                                        sprite_sheet.width,
+                                                        sprite_sheet.height)
+
+    assert sprite_sheet.draw_kwargs(
+        x, y, subsprite_column, subsprite_row) == {"source": sprite_sheet.image,
+                                                   "dest": sprite_sheet.desired_on_screen_position(x, y),
+                                                   "area": sprite_sheet.curr_subsprite()}
